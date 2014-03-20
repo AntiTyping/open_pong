734381257aea6919a2298b8f06f05b88cfb13361
Andy Pliszka <andy@isp44.com>
2014-03-19 21:56:12 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenPong::Application.config.secret_key_base = 'e5f60c2e171ae09b467f9015db41d4348713312db4cbc4cc9fbf0dfce9cb2c3ac0623844096cb7bd52d37bc0fb83ce42e601e27a715df18b1e20f4521d1826d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenPong::Application.config.session_store :cookie_store, key: '_open_pong_session'
